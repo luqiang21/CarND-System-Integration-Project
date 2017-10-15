3ae66224c89f2716e86130b036251ec9292a5dfc
Qiang Lu <buaaluqiang@hotmail.com>
2017-10-15 09:58:43 -0600

    planning (updater):2hz, actuation(dbw): 50hz

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -55,7 +55,7 @@ class WaypointUpdater(object):
 		self.loop()
 
 	def loop(self):
-		rate = rospy.Rate(10)
+		rate = rospy.Rate(2)
 
 		while not rospy.is_shutdown():
 			rate.sleep() # With the help of Rate's method sleep(), it offers a convenient way for looping at the desired rate. With its argument of 10, we should expect to go through the loop 10 times per second (as long as our processing time does not exceed 1/10th of a second!)
